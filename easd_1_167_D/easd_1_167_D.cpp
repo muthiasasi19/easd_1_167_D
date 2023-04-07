57dc9c10c6c5e44608e28422fc0022fd249b755f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-04-06 20:12:39 -0700

    input variabel

---
+++ easd_1_167_D/easd_1_167_D.cpp
@@ -0,0 +1,20 @@
+/*1. Algoritma dibutuhkan untuk menyelesaikan suatu masalah, karena algoritma itu merupakan urutan langkah-langkah logis yang disusun atau dirancang untuk menyelesaikan masalah dengan cepat dan efisien.
+  2. - statis (Array)
+	 - Dinamis (Linked List)
+  3. Faktor yang memepengaruhi program:
+	- Size of th einput (ukuran input)
+	- Compiler (penerjemah)
+	- Speed of machine
+	- operating system
+  4. 
+  5. Quadratic : bubble sort, insertion sort, dan selection sort
+     Loglinear : merge sort, quick sort, dan shell sort(dalam beberapa kondisi)*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX = 87; // hasil dari 67 + 20 -2 x 15 + 10 + 20
+    int sasi[MAX], n;
+}
+
